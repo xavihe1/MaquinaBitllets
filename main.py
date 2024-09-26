45fd9b638d2858f6ff1d201dc63d557eccfee795
xavihe1 <xavier.heredia.7e7@itb.cat>
2024-09-26 17:30:26 +0200

    estructura

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+"""
+LA MÀQUINA DE BITLLETS DE METRO
+"""
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def opcions_menu():
+    print("----------")
+    print("Quin bitllet desitja adquirir?")
+    print("1. Bitllet senzill")  # 2.40€ (1a zona)
+    print("2. T-Casual")  # 11.35€ (1a zona)
+    print("3. T-Usual")  # 40.00€ (1a zona)
+    print("4. T-Familiar")  # 10.00€ (1a zona)
+    print("5. T-Jove")  # 80.00€ (1a zona)
+
+    # Els preus de cada zona són el preu de l'anterior * 1.25
+
+def opcions_zones():
+    print("De quantes zones vol el bitllet?")
+    print("1. 1 zona")
+    print("2. 2 zones")
+    print("3. 3 zones")
+    print("4. 4 zones")
+    print("5. 5 zones")
+    print("6. 6 zones")
+
+def pagar():
+    print("Ha comprat _ bitllet/s, ha de pagar _€")
+    print("Introdueixi monedes o bitllets vàlids de EURO")
+    # Només es pot pagar amb: 0.05€, 0.10€, 0.20€, 0.50€, 1€, 2€, 5€, 10€, 20€ i 50€
+
+def menu():
+    opcions_menu()
+    opcio = int
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def tiquet():
+    print("---TIQUET---")
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
